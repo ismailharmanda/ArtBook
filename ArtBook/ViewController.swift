4305b50ebb56398ac0c024b0ed226da812e46e0f
İsmail Harmanda <iharmanda@hotmail.com>
2023-10-26 21:27:33 +0300

    initial commit

---
+++ ArtBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ArtBook
+//
+//  Created by ismail harmanda on 26.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
